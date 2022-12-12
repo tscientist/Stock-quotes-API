ec88cd036ac0a662674c2ae33467f4f4d651e507
Andrei Majada <andrei.majada@somosquiq.com>
2022-12-11 22:00:29 -0300

    Adding swagger autogen to stock-api

---
+++ stock-service/swagger.js
@@ -0,0 +1,17 @@
+const swaggerAutogen = require('swagger-autogen')()
+
+const doc = {
+    info: {
+      title: 'api-service',
+      description: 'description',
+    },
+    host: 'localhost:3002',
+    schemes: ['http'],
+  };
+
+const outputFile = './swagger_output.json'
+const endpointsFiles = ['./src/routes/index.js']
+
+swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
+    require('./index.js')
+})
